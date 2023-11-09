0f9596571d302108ec7e63f606a111097a2acc0d
wilmer <wilmer.arango.a@gmail.com>
2023-11-09 15:57:23 +0100

    Start

---
+++ src/main/java/com/experis/course/springPizzeriaCrud/Model/Pizze.java
@@ -0,0 +1,8 @@
+package com.experis.course.springPizzeriaCrud.Model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Pizze {
+
+}
---
+++ src/test/java/com/experis/course/springPizzeriaCrud/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.course.springPizzeriaCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
